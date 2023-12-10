c42f01ad5e4ddd6b726827489dc4d874318502e4
Sahil Sharma <sahilbhardwaj549@gmail.com>
2023-12-10 13:53:57 +0530

    initial commit , Add - login screen design

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <TextInput placeholder='Enter Name'/>
+      <TextInput placeholder='Enter Password'
+      secureTextEntry = {true}/>
+     <Button title='Login'/>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
